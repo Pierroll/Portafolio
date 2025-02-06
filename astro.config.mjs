b13827d684adffcf7854432f54b178ca603d36fb
PIERROLL <ppiero119@gmail.com>
2025-02-06 16:44:11 -0500

    feat: mejoras SEO - agregado JsonLD y configuración de sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite'
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
   site: 'https://pmorris-portafolio.onrender.com',
-  integrations: [sitemap()]
+  integrations: [
+    sitemap()
+  ]
 });
